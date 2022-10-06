72afb24ae69ab62e0165e13e925532f5bc06910d
142spp <ldg142s@gmail.com>
2022-10-06 17:59:27 +0900

    2502

---
+++ 초급_트랙_1차시/2502_떡_먹는_호랑이/142spp.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<pair<int,int>> table(31);
+pair<int,int> fibo(int x){
+    if(table[x].first || table[x].second) return table[x];
+    pair<int,int> p = {0,0};
+    if(x==1){ p.first++; }
+    else if (x==2){ p.second++; }
+    else if(!table[x].first && !table[x].second){
+        p.first += fibo(x-1).first;
+        p.second += fibo(x-1).second;
+        p.first += fibo(x-2).first;
+        p.second += fibo(x-2).second;
+    }
+    table[x] = p;
+    return table[x];
+}
+
+int main(){
+    int x,y ; cin >> x >> y;
+    pair<int,int> p = fibo(x);
+    for(int i=1; i<=y; i++){
+        for(int j=i; j<=y; j++){
+            if(p.first*i + p.second*j == y) {
+                cout << i << "\n" << j << "\n";
+                return 0;
+            }
+            if(p.first*i + p.second*j > y) break; 
+        }
+    }
+    return 0;
+}
